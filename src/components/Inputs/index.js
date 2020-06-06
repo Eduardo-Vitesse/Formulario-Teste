703676cb60e12e61626d52fa4ec95bfd1b4c1c31
Filipe Lopes <filiperocklopes@gmail.com>
2020-06-06 15:19:02 -0300

    Variação de estilo de focus e filled nos inputs

--- src/components/Inputs/index.js
+++ src/components/Inputs/index.js
@@ -1,29 +1,28 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 
 import { Input } from "./style";
 
 const InputFields = (props) => {
-	const [active, setActive] = useState(false);
-	const activeRef = useRef(null);
-
-	const handleActive = () => {
-		setActive(true);
-	};
-
-	useEffect(() => {
-		if (active === true) {
-			activeRef.current.style.border = "1px solid #DA7F16";
-			activeRef.current.style.backgroundColor = "#3A3A3A;";
-			activeRef.current.style.color = "#DA7F16";
-		}
-	}, [active]);
+	const [focus, setFocus] = useState(false);
+	const [filled, setFilled] = useState(false);
 
 	return (
 		<Input
-			ref={activeRef}
 			type={props.type}
 			placeholder={props.placeholder}
-			onFocus={handleActive}
+			focus={focus}
+			filled={filled}
+			onFocus={() => {
+				setFocus(true);
+			}}
+			onBlur={(e) => {
+				setFocus(false);
+				if (e.target.value) {
+					setFilled(true);
+				} else {
+					setFilled(false);
+				}
+			}}
 		/>
 	);
 };
--- src/components/Inputs/style.js
+++ src/components/Inputs/style.js
@@ -7,9 +7,15 @@ export const Input = styled.input`
 	font-size: 18px;
 	padding-left: 15px;
 	font-family: "Montserrat", sans-serif;
-	border: 1px solid #868686;
+	border: ${({ focus, filled }) =>
+		focus
+			? "1px solid #DA7F16"
+			: filled
+			? "1px solid #fff"
+			: "1px solid #868686"};
+	color: ${({ focus }) => (focus ? "#DA7F16" : "#ffffff")};
 	background: #3a3a3a;
-	color: #ffffff;
+
 	&:nth-child(2) {
 		margin-top: 37px;
 	}
